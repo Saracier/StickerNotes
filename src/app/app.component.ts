1e78732b0c1a6acab68e4cba44105ddf756ab5e1
Saracier <105667230+Saracier@users.noreply.github.com>
2023-06-15 11:12:22 +0200

    feat: add title input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  inputValue = '';
+  textInputValue = '';
+  // titleInputValue = '';
   inputContainsSomething = false;
-  notes: string[] = [];
+  notes: { id: number; title: string; text: string }[] = [];
+  counter: number = 1;
 
-  addNewNote() {
-    if (this.inputValue.length === 0) return;
-    this.notes.push(this.inputValue);
-    this.inputValue = '';
+  addNewNote(titleInputValue: HTMLInputElement) {
+    if (this.textInputValue.length === 0 || titleInputValue.value.length === 0)
+      return;
+    this.notes.push({
+      id: this.counter,
+      title: titleInputValue.value,
+      text: this.textInputValue,
+    });
+    this.counter++;
+    this.textInputValue = '';
+    // this.titleInputValue = '';
   }
 
   evaluateInput() {
-    this.inputContainsSomething = Boolean(this.inputValue.length > 0);
+    this.inputContainsSomething = Boolean(this.textInputValue.length > 0);
+    // this.inputContainsSomething = Boolean(this.titleInputValue.length > 0);
+  }
+
+  onDeleteSingleNote(deleteObject: { id: number }) {
+    const numberInArray = this.notes.findIndex((element) => {
+      return element.id === deleteObject.id;
+    });
+    if (numberInArray < 0)
+      throw new Error(
+        'something went wrong. Note for delete exeeded index of array'
+      );
+    this.notes.splice(numberInArray, 1);
   }
 
   // onUpdateInput(event: Event) {
-  //   this.inputValue = (<HTMLInputElement>event.target).value;
-  //   this.inputContainsSomething = Boolean(this.inputValue.length > 0);
+  //   this.textInputValue = (<HTMLInputElement>event.target).value;
+  //   this.inputContainsSomething = Boolean(this.textInputValue.length > 0);
   // }
 }
--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
-  styleUrls: ['./note.component.scss']
+  styleUrls: ['./note.component.scss'],
 })
 export class NoteComponent {
-
+  @Input() note: { id: number; title: string; text: string } = {
+    id: NaN,
+    title: '',
+    text: '',
+  };
+  @Output() deleteNote = new EventEmitter<{ id: number }>();
+  constructor() {}
+  deleteHandler() {
+    this.deleteNote.emit({ id: this.note.id });
+  }
 }
