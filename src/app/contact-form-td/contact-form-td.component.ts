33a3899de864634339a1a73812fdffc4c1465de7
Saracier <105667230+Saracier@users.noreply.github.com>
2023-06-21 18:40:25 +0200

    feat: add template driven form

--- src/app/contact-form-td/contact-form-td.component.ts
+++ src/app/contact-form-td/contact-form-td.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form-td',
   templateUrl: './contact-form-td.component.html',
-  styleUrls: ['./contact-form-td.component.scss']
+  styleUrls: ['./contact-form-td.component.scss'],
 })
 export class ContactFormTdComponent {
+  messageInput: string;
+  @ViewChild('formTDElement') formTDElement: NgForm;
+  defoultAnswer = 'yes';
+  genders = ['male', 'female'];
+  // onSubmit(formElement: NgForm) {
+  //   console.log(formElement);
+  // }
 
+  onSubmit() {
+    console.log(this.formTDElement.value.userSendUsTicket);
+    this.formTDElement.reset();
+  }
 }
