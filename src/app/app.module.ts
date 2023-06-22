8c87c7af554f6529c0d05b9a9a1765ff3181e19b
Saracier <105667230+Saracier@users.noreply.github.com>
2023-06-22 17:06:28 +0200

    feat: add feactive type contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +28,7 @@ import { ContactFormRComponent } from './contact-form-r/contact-form-r.component
     ContactFormTdComponent,
     ContactFormRComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/contact-form-r/contact-form-r.component.ts
+++ src/app/contact-form-r/contact-form-r.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form-r',
   templateUrl: './contact-form-r.component.html',
-  styleUrls: ['./contact-form-r.component.scss']
+  styleUrls: ['./contact-form-r.component.scss'],
 })
 export class ContactFormRComponent {
+  genders = ['male', 'female'];
+  forbiddenUsernames = ['Goku', 'Gohan'];
+  signupForm: FormGroup;
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNamesCheck.bind(this),
+        ]),
+        gender: new FormControl('male'),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      marketing: new FormControl('yes'),
+      message: new FormControl(null, Validators.required),
+    });
+  }
 
+  onSubmit() {
+    console.log(this.signupForm);
+    this.signupForm.reset();
+  }
+
+  forbiddenNamesCheck(control: FormControl): { [s: string]: boolean } | null {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+    return null;
+  }
 }
