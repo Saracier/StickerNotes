f53d8d2251012fb34ed16fb239c84d033e37490d
Saracier <105667230+Saracier@users.noreply.github.com>
2023-07-06 11:50:02 +0200

    feat: add css for everything

--- src/app/note/focus.directive.ts
+++ src/app/note/focus.directive.ts
@@ -14,12 +14,15 @@ export class FocusDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   @HostBinding('style.background-color') backColor = 'transparent';
+  @HostBinding('style.color') textColor = 'white';
 
   @HostListener('mouseenter') mouseIsInside(eventData: Event) {
     this.backColor = 'yellow';
+    this.textColor = 'black';
   }
   @HostListener('mouseleave') mouseIsGone(eventData: Event) {
     this.backColor = 'transparent';
+    this.textColor = 'white';
   }
 
   // @HostListener('mouseenter') mouseIsInside(eventData: Event) {
